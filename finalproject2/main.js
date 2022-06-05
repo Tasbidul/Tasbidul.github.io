52d8d5fa43984743b40044b9c44ab17d8648eec2
Tasbidul <100880125+Tasbidul@users.noreply.github.com>
2022-06-05 16:20:28 -0400

    Add files via upload

---
+++ finalproject2/main.js
@@ -0,0 +1,90 @@
+let checkBtn = document.getElementById('check');
+let output = document.getElementById('outputtext');
+let guessSlot = document.getElementById('guesses');
+let remaining = document.getElementById('remaining');
+let restart = document.getElementById('restart');
+let score = document.getElementById('score');
+let img = document.getElementById('img');
+let numGuessesLeft = 10
+let highScore = 10
+let hScore = 0
+let randomNumber = parseInt((Math.random()*100)+1)
+
+const storageInput = document.querySelector('.storage');
+const name = document.querySelector('.name');
+const nBtn = document.querySelector('.btn');
+const hs = document.querySelector('.hs');
+const storedInput = localStorage.getItem('textinput')
+if(storedInput) {
+  name.textContent = storedInput
+}
+storageInput.addEventListener('input', letter => {
+  name.textContent = letter.target.value
+})
+const saveToLocalStorage = () => {
+  localStorage.setItem('textinput', name.textContent)
+}
+nBtn.addEventListener('click', saveToLocalStorage)
+
+let usedValues = [];
+checkBtn.addEventListener('click', function(){
+let input = document.getElementById('userInput').value;
+
+
+
+if (isNaN(input)){
+    output.innerHTML = 'Please enter a valid number.'
+} else if (input < 1) {
+    output.innerHTML = 'Please enter a number greater than 1!'
+} else if (input > 100){
+    output.innerHTML = 'Please enter a number less than 100!'
+} else if (usedValues.includes(input)){
+    output.innerHTML = 'You already used this number!'
+} else {
+    usedValues.push(input);
+    if (input == randomNumber){
+      checkBtn.style.display = "none";
+      img.src = "https://cdn-icons-png.flaticon.com/512/3076/3076886.png"
+      output.innerHTML = `Congrats! you guessed right, your number was ${randomNumber}.`
+      guessSlot.innerHTML += `${input} `;
+      restart.innerHTML = 'Play Again';
+      hScore = 10-numGuessesLeft+1;
+      if (hScore < highScore) {
+        highScore = hScore
+
+      };
+      score.innerHTML = `${highScore} attempts used.`
+      localStorage.setItem('hs', highScore);
+      hs.innerHTML = localStorage.getItem('hs')
+    } else if (input < randomNumber){
+      output.innerHTML = "You guessed too low!"
+      guessSlot.innerHTML += `${input} `;
+      numGuessesLeft--
+      remaining.innerHTML = `${numGuessesLeft}`
+    }; if (input > randomNumber){
+      output.innerHTML = "You guessed too high!"
+      guessSlot.innerHTML += `${input} `;
+      numGuessesLeft--
+      remaining.innerHTML = `${numGuessesLeft}`
+    }; if (numGuessesLeft === 0) {
+      img.src = "https://cdn-icons-png.flaticon.com/512/3076/3076989.png"
+      output.innerHTML = `Game Over! The number was ${randomNumber}.`
+      checkBtn.style.display = "none";
+      restart.innerHTML = 'Try Again';
+    }
+  }
+});
+hs.innerHTML = localStorage.getItem('hs')
+console.log(localStorage);
+
+restart.addEventListener('click', function(){
+  randomNumber = parseInt((Math.random()*100)+1)
+  img.src = "https://cdn-icons-png.flaticon.com/512/3076/3076945.png"
+  usedValues = [];
+  remaining.innerHTML = `10`
+  restart.innerHTML = 'Restart';
+  checkBtn.style.display = "";
+  guessSlot.innerHTML = '';
+  output.innerHTML = 'Enter a number below';
+  numGuessesLeft = 10
+})
